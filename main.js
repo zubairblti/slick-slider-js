fb4a3c3f41fbf878c91488f9d771143d7d138326
Muhammad Zubair <124900011+zubairblti@users.noreply.github.com>
2024-12-16 22:01:29 +0500

    Code has been Update

---
+++ main.js
@@ -0,0 +1,36 @@
+$('.responsive').slick({
+    dots: true,
+    focusOnSelect: false,
+    rows: 0,
+    autoplay: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: false,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
